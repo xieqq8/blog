9c0168bc3bb39ea94d4bfbf038782c32fb302116
xieqingqing <xieqingqing@kuaxue.com>
2017-07-20 17:53:49 +0800

    xie   基于elasticsearch进行博客全文索引，并且博客数据存放在elasticsearch中，需要配置elasticsearch环境，并初始化blog索引。 脚本在index_init.sh中，执行两个curl命令即可。
    ElasticSearch是一个基于Lucene的搜索服务器。它提供了一个分布式多用户能力的全文搜索引擎，基于RESTful web接口。Elasticsearch是用Java开发的，并作为Apache许可条款下的开放源码发布，是当前流行的企业级搜索引擎。设计用于云计算中，能够达到实时搜索，稳定，可靠，快速，安装使用方便。
    
    一个网站或应用程序，并要添加搜索功能，但是想要完成搜索工作的创建是非常困难的。我们希望搜索解决方案要运行速度快，我们希望能有一个零配置和一个完全免费的搜索模式，我们希望能够简单地使用JSON通过HTTP来索引数据，我们希望我们的搜索服务器始终可用，我们希望能够从一台开始并扩展到数百台，我们要实时搜索，我们要简单的多租户，我们希望建立一个云的解决方案。因此我们利用Elasticsearch来解决所有这些问题以及可能出现的更多其它问题。

---
+++ src/main/resources/init_index.sh
@@ -0,0 +1,34 @@
+curl -X PUT http://localhost:9200/blog -d '{
+                                               "settings" : {
+                                                   "index" : {
+                                                       "number_of_shards" : 3,
+                                                       "number_of_replicas" : 0
+                                                   }
+                                               }
+                                           }'
+
+curl -X POST http://localhost:9200/blog/blog/_mapping -d '
+{
+      "properties": {
+        "id":    { "type": "text", "index":"no"  },
+      "title":    { "type": "text","analyzer": "ik_smart",
+                "search_analyzer": "ik_smart"  },
+        "secondTitle":    { "type": "text","analyzer": "ik_smart",
+                "search_analyzer": "ik_smart"  },
+        "catalog":    { "type": "keyword"  },
+        "auther":    { "type": "keyword", "index":"no"  },
+        "img":    { "type": "text", "index":"no"  },
+        "md":     { "type": "text","analyzer": "ik_smart",
+                "search_analyzer": "ik_smart"},
+        "display":  {
+          "type":   "text",
+          "index":"no"
+
+        },
+        "publishTime":  {
+          "type":   "date",
+          "format": "strict_date_optional_time||epoch_millis"
+        }
+      }
+}
+'
