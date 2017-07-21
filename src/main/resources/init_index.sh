5d35de1d45ee2268ac0955417f3e96a28a52a93e
xieqingqing <xieqingqing@kuaxue.com>
2017-07-21 17:06:54 +0800

    xie db

--- src/main/resources/init_index.sh
+++ src/main/resources/init_index.sh
@@ -7,28 +7,45 @@ curl -X PUT http://localhost:9200/blog -d '{
                                                }
                                            }'
 
-curl -X POST http://localhost:9200/blog/blog/_mapping -d '
-{
+curl -X POST http://localhost:9200/blog/blog/_mapping -d '{
       "properties": {
-        "id":    { "type": "text", "index":"no"  },
-      "title":    { "type": "text","analyzer": "ik_smart",
-                "search_analyzer": "ik_smart"  },
-        "secondTitle":    { "type": "text","analyzer": "ik_smart",
-                "search_analyzer": "ik_smart"  },
-        "catalog":    { "type": "keyword"  },
-        "auther":    { "type": "keyword", "index":"no"  },
-        "img":    { "type": "text", "index":"no"  },
-        "md":     { "type": "text","analyzer": "ik_smart",
-                "search_analyzer": "ik_smart"},
-        "display":  {
-          "type":   "text",
-          "index":"no"
-
-        },
-        "publishTime":  {
-          "type":   "date",
-          "format": "strict_date_optional_time||epoch_millis"
-        }
+            "id":    {
+                "type": "text",
+                "index":"no"
+            },
+            "title": {
+                "type": "text",
+                "analyzer": "ik_smart",
+                "search_analyzer": "ik_smart"
+            },
+            "secondTitle":    {
+                "type": "text",
+                "analyzer": "ik_smart",
+                "search_analyzer": "ik_smart"
+            },
+            "catalog":    {
+                "type": "keyword"
+            },
+            "auther":    {
+                "type": "keyword",
+                "index":"no"
+            },
+            "img":    {
+                "type": "text",
+                "index":"no"
+            },
+            "md":     {
+                "type": "text",
+                "analyzer": "ik_smart",
+                "search_analyzer": "ik_smart"
+            },
+            "display":  {
+                "type":   "text",
+                "index":"no"
+            },
+            "publishTime":  {
+                "type":   "date",
+                "format": "strict_date_optional_time||epoch_millis"
+            }
       }
-}
-'
+}'
