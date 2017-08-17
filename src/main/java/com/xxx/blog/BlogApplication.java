279b5b91475aee10ea431285843abc3651603e7f
xieqingqing <xieqingqing@kuaxue.com>
2017-08-17 15:34:51 +0800

    xie 编译发布版本，改bug spring:   jmx:     default-domain: xxxblog

--- src/main/java/com/xxx/blog/BlogApplication.java
+++ src/main/java/com/xxx/blog/BlogApplication.java
@@ -2,11 +2,29 @@ package com.xxx.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class BlogApplication {
+//@SpringBootApplication
+//public class BlogApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(BlogApplication.class, args);
+//	}
+//}
 
-	public static void main(String[] args) {
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class BlogApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args){
 		SpringApplication.run(BlogApplication.class, args);
+
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BlogApplication.class);
+	}
+
 }
